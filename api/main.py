de07360ccb8d367882567d37396e25921ec2faa7
SKAkramali <shaikakramali29@gmail.com>
2026-07-12 23:06:24 +0530

    fix: altered main.py

--- api/main.py
+++ api/main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from ipl!")
+from fastapi import FastAPI
+
+from core.database import Base, engine
+from api.routs.player import router as player_router
+
+# 1. IMPORT ALL DATABASE MODELS HERE SO SQLALCHEMY REGISTERS THEM
+from models.Player import Player
+from models.Team import Team          #  This registers the 'Team' table
+from models.Scorecard import Scorecard
+from models.Tournment import Tournament # (Check your filename spelling)
+from models.User import User
 
 
-if __name__ == "__main__":
-    main()
+Base.metadata.create_all(engine)
+
+app = FastAPI()
+app.include_router(player_router)
