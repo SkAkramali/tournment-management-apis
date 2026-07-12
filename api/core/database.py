d907abc3fc429a1ff492f3b85fe59ebdb4ef64a2
SKAkramali <shaikakramali29@gmail.com>
2026-07-12 22:59:10 +0530

    added database configuration

--- api/core/database.py
+++ api/core/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:akram7282@localhost:3306/fastapi"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:akram7282@localhost:3306/fastapi_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
@@ -11,3 +11,10 @@ sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
   pass
+
+def get_db():
+  db = sessionLocal()
+  try: 
+    yield db
+  finally:
+    db.close()
