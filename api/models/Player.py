b4c6a9fa31d41db83cb0f1fdbaf4031b35d19ec8
SKAkramali <shaikakramali29@gmail.com>
2026-07-12 23:00:26 +0530

    fix: fixed the bug in keys

--- api/models/Player.py
+++ api/models/Player.py
@@ -1,25 +1,20 @@
-from api.core.database import Base
-from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import String
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
+from core.database import Base
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 
 class Player(Base):
-  __tablename__ = "players"
+    __tablename__ = "players"
 
-  id: Mapped[int] = mapped_column(primary_key=True)
-  name: Mapped[str] = mapped_column(String(100), nullable=False)
-  age: Mapped[int]
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    age: Mapped[int] = mapped_column(nullable=False)
 
-  role: Mapped[str] = mapped_column(
-        String(50)
-  )
+    role: Mapped[str] = mapped_column(String(50))
 
-  team_id: Mapped[int] = mapped_column(
-        ForeignKey("teams.id")
-  )
+    team_id: Mapped[int] = mapped_column(ForeignKey("teams.id"))
 
-  team = relationship(
+    team = relationship(
         "Team",
-        back_populates="players"
-  )
+        back_populates="players",
+    )
