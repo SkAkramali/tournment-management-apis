5efcc5ecdaf0d133b838bb7049bedfd95253ef86
SKAkramali <shaikakramali29@gmail.com>
2026-07-14 16:32:42 +0530

    added schema to update team

--- api/schemas/tournment.py
+++ api/schemas/tournment.py
@@ -11,3 +11,9 @@ class TournamentCreate(BaseModel):
   name: str = Field(..., max_length=50)
   sport: str = Field(..., max_length=50)
   season: str = Field(..., max_length=50)
+
+
+class TournamentUpdate(BaseModel):
+  name: str | None = Field(default=None, max_length=50)
+  sport: str | None = Field(default=None, max_length=50)
+  season: str | None = Field(default=None, max_length=50)
