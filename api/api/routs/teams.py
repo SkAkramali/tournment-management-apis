6fbe7e6dd0abb6ad5b4916eda81aaba04c3c0b00
SKAkramali <shaikakramali29@gmail.com>
2026-07-14 16:30:54 +0530

    feat: added route delete team

--- api/api/routs/teams.py
+++ api/api/routs/teams.py
@@ -1,6 +1,5 @@
 from core.database import Base
 from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import ForeignKey
 from fastapi import APIRouter, Depends, HTTPException
 from schemas.team import TeamCreate, TeamResponse
 from sqlalchemy.orm import Session
@@ -33,6 +32,28 @@ def get_team(team_id: int, db: Session = Depends(get_db)) -> TeamResponse:
 def create_team(team: TeamCreate, db: Session = Depends(get_db)) -> TeamResponse:
   db_team = Team(**team.dict())
   db.add(db_team)
+  db.commit()
+  db.refresh(db_team)
+  return db_team
+
+@router.delete("/{team_id}", status_code=204) 
+def delete_team(team_id: int, db: Session = Depends(get_db)):
+  stmt  = select(Team).where(Team.id == team_id)
+  team = db.execute(stmt).scalar_one_or_none()
+  if not team:
+    raise HTTPException(status_code=404, detail="Team not found")
+  db.delete(team)
+  db.commit()
+
+
+@router.put("/updateDetails/", response_model=TeamResponse, status_code=200) 
+def upadate_details(team: TeamResponse, db: Session = Depends(get_db)) -> TeamResponse:
+  stmt = select(Team).where(Team.id == team.id)
+  db_team = db.execute(stmt).scalar_one_or_none()
+  if not db_team:
+    raise HTTPException(status_code=404, detail="Team not found")
+  for key, value in team.dict().items():
+    setattr(db_team, key, value)
   db.commit()
   db.refresh(db_team)
   return db_team
